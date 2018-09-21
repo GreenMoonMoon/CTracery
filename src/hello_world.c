9b02d9bd3c19f9ba94493fb312d1934dbf2d2901
Josue Boisvert <josue.boisvert@reelfx.com>
2018-09-21 12:29:11 -0400

    add header file

--- src/hello_world.c
+++ src/hello_world.c
@@ -1,9 +1,37 @@
-#include <stdio.h>
+#include "hello_world.h"
 
 int main()
 {
-    /*General comments about this func*/
-    printf("Hello world\n");
+    /*General comments about this function*/
+    // printf("Hello world\n");
+    testMalloc(10);
 
     return 0;
+}
+
+void testMalloc(int size)
+{
+    int *darray = (int *)malloc(size * sizeof(int));
+    if (darray == NULL)
+    {
+        /*Always check for allocation failure, since allocation is not garenteed*/
+        printf("malloc failed");
+        exit(1);
+    }
+    int darray_size = size * sizeof(int);
+    int array[10];
+    printf("size of 10 int array: %d\n", (int)sizeof(array));
+    printf("supposed size of %d int array: %d\n", size, darray_size);
+    printf("size of darray: %d\n", (int)sizeof(darray));
+    printf("size of int: %d\n", (int)sizeof(int));
+
+    int i;
+    for (i = 0; i < size; i++)
+    {
+        darray[i] = i;
+    }
+    printf("%d\n", darray[10]);
+    darray = realloc(darray, 11);
+    printf("%d\n", darray[10]);
+    free(darray);
 }
