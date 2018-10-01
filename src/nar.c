24085fe5bd007834a02f06be2556e3f4d49f84ef
GreenMoonMoon <josue.boisvert@gmail.com>
2018-09-30 22:51:04 -0400

    change component structure

--- src/nar.c
+++ src/nar.c
@@ -1,26 +1,31 @@
 /*Narrative Engine structure*/
+#include <stdlib.h>
+#include <stdarg.h>
 
-struct Component
+typedef struct Component
 {
-    char *id;
-    char *values;
+    char id;
+    char **values;
     int current;
-};
+} Component;
 
-struct State
-{
-    struct Component *components;
-    struct Action *actions;
-};
+// struct State
+// {
+//     struct Component *components;
+//     struct Action *actions;
+// } State;
  
-struct Action
-{
-    struct Component component;
-    int value;
-};
+// struct Action
+// {
+//     struct Component component;
+//     int value;
+// } Action;
  
-struct Component NewComponent(char *id, char *values[]){
-    struct Component c;
+struct Component NewComponent(char *id, int count, ...){
+    va_list ap;
+    va_start()
+    * values = malloc(sizeof())
+    Component c = (Component){.id = "Being", .values = [](char*)[]{"alive", "dead"}, .current = 0};
     
     return c;
 }
